016e6bfb07374ebc57039b54eeebb2ce3396a459
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-05-24 09:00:58 -0400

    prior to pull

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,50 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    private int earnedMarks;
+
+    private int possibleMarks;
+
+    private String submitterName;
+
+    private String letterGrade;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade(int earnedMarks, int possibleMarks) {
+        String result;
+        double percentage = (double) earnedMarks / (double) possibleMarks;
+        if (percentage >= 0.9) {
+            result = "A";
+        } else if (percentage >= 0.8) {
+            result = "B";
+        } else if (percentage >= 0.7) {
+            result = "C";
+        } else if (percentage >= 0.6) {
+            result = "D";
+        } else {
+            result = "F";
+        }
+        return result;
+    }
 
 }
