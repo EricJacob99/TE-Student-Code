31da22d91fe18e84d7c48783a04c49aea913f584
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-05-28 09:38:13 -0400

    prior to pull prior to pull

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -2,4 +2,62 @@ package com.techelevator;
 
 public class Airplane {
 
+    private String planeNumber;
+
+    private int totalFirstClassSeats;
+
+    private int bookedFirstClassSeats;
+
+    private int totalCoachSeats;
+
+    private int bookedCoachSeats;
+
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats() {
+        return totalFirstClassSeats;
+    }
+
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
+
+    public int getAvailableFirstClassSeats() {
+        int availableFirstClassSeats = totalFirstClassSeats - bookedFirstClassSeats;
+        return availableFirstClassSeats;
+    }
+
+    public int getAvailableCoachSeats() {
+        int availableCoachSeats = totalCoachSeats - bookedCoachSeats;
+        return availableCoachSeats;
+    }
+
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+    public boolean reserveSeats (boolean forFirstClass, int totalNumberOfSeats) {
+        if (forFirstClass && getAvailableFirstClassSeats() >= totalNumberOfSeats) {
+            bookedFirstClassSeats = bookedFirstClassSeats + totalNumberOfSeats;
+            return true;
+        }else if (!forFirstClass && getAvailableCoachSeats() >= totalNumberOfSeats){
+            bookedCoachSeats = bookedCoachSeats + totalNumberOfSeats;
+            return true;
+        }
+        return false;
+
+
+    }
 }
--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -2,4 +2,43 @@ package com.techelevator;
 
 public class Television {
 
+    private boolean isOn;
+
+    private int currentChannel;
+
+    private int currentVolume;
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void turnOff() {
+
+    }
+    public void turnOn() {
+
+    }
+    public void changeChannel(int newChannel) {
+
+    }
+    public void channelUp() {
+
+    }
+    public void channelDown() {
+
+    }
+    public void raiseVolume() {
+
+    }
+    public void lowerVolume() {
+
+    }
 }
