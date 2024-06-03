f6bc652d2efa7f763682068e22ffe5a698c5cfc1
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-06-03 09:09:29 -0400

    prior to pull

--- module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
+++ module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
@@ -1,13 +1,15 @@
 package com.techelevator;
 
 public class SquareWall extends Wall{
+
+    private int sideLength;
     public SquareWall(String name, String color, int sideLength) {
         super(name, color);
-
+        this.sideLength = sideLength;
     }
 
     @Override
     public int getArea() {
-        return 0;
+        return (sideLength*sideLength);
     }
 }
--- module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/TriangleWall.java
+++ module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/TriangleWall.java
@@ -1,12 +1,18 @@
 package com.techelevator;
 
 public class TriangleWall extends Wall{
+
+    private int base;
+    private int height;
+
     public TriangleWall(String name, String color, int base, int height) {
         super(name, color);
+        this.base = base;
+        this.height = height;
     }
 
     @Override
     public int getArea() {
-        return 0;
+        return ((base*height)/2);
     }
 }
