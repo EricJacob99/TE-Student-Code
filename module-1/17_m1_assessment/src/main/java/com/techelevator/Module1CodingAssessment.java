14c2e0746dc9c68e1ec1b8e4d76906e0a52c3364
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-06-05 09:37:22 -0400

    step 4 and 5 complete

--- module-1/17_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.*;
+
 public class Module1CodingAssessment {
 
 	public static void main(String[] args) {
@@ -8,10 +10,17 @@ public class Module1CodingAssessment {
 	}
 
 	private void run() {
+           //start coding here... You will call other class or classes from here as needed, or write additional methods.
 
-           //start coding here... You will call other class or classes from here as needed, or write additional methods. 
+		MovieRental dieHardRental = new  MovieRental("Die Hard", "BluRay", true);
+		MovieRental jurassicParkRental = new  MovieRental("Jurassic Park", "DVD", false);
+		MovieRental bambiRental = new  MovieRental("Bambi", "VHS", false);
 
+		List<MovieRental> rentals= new ArrayList<>();
 
+		rentals.add(dieHardRental);
+		rentals.add(jurassicParkRental);
+		rentals.add(bambiRental);
 	}
 
 }
