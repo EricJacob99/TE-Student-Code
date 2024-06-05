dd2453673d5009436bf32c9db5972033cb882a8f
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-06-05 09:24:53 -0400

    step 1 and 2 complete

---
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/MovieRental.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+public class MovieRental {
+    private String title;
+    private String format;
+    private Boolean isPremium;
+    private Double rentalPrice;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public Boolean getPremium() {
+        return isPremium;
+    }
+
+    public Double getRentalPrice() {
+        double price = 0;
+
+        if (format == "VHS"){
+            price = price + 0.99;
+        }
+        if (format == "DVD"){
+            price = price + 1.99;
+        }
+        if (format == "BluRay"){
+            price = price + 2.99;
+        }
+        if (isPremium && price > 0) {
+            price = price + 1.00;
+        }
+
+        return price;
+    }
+
+    public MovieRental(String title, String format, Boolean isPremium) {
+        this.title = title;
+        this.format = format;
+        this.isPremium = isPremium;
+    }
+}
